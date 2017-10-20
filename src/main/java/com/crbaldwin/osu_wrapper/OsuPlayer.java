a54fdeba35b9226069726e3ac7d3e542db2d0399
crbaldwin <charlesr.baldwinn@gmail.com>
2017-10-20 00:24:23 -0500

    Initial Commit

--- src/main/java/com/crbaldwin/osu_wrapper/OsuPlayer.java
+++ src/main/java/com/crbaldwin/osu_wrapper/OsuPlayer.java
@@ -15,73 +15,49 @@ public class OsuPlayer {
     private final String GET_USER_RECENT = "https://osu.ppy.sh/api/get_user_recent?k=";
 
     //Osu User Variables
-    private String username, country, profile_url, avatar_url;
-    private int user_id, pp_rank, pp_country_rank, playcount, count300, count100, count50, count_rank_ss, count_rank_s, count_rank_a;
+    private String username, country, profile_url, avatar_url, uname, key;
+    private int user_id, pp_rank, pp_country_rank, playcount, count300, count100, count50, count_rank_ss, count_rank_s, count_rank_a, user_ID;
     private double pp_raw, accuracy, level;
     private long ranked_score, total_score;
+    private Osu.OsuGameMode gmode;
 
 
 
     OsuPlayer(String API_KEY, String name, Osu.OsuGameMode gameMode) throws OsuUserException, IOException, OsuGamemodeException {
-        JSONObject user = new JSONObject();
-        if(gameMode == Osu.OsuGameMode.OSU) {
-            user = REQUEST.callApi(GET_USER + API_KEY + "&u=" + name + "&m=0");
-        }
-        else if(gameMode == Osu.OsuGameMode.TAIKO) {
-            user = REQUEST.callApi(GET_USER + API_KEY + "&u=" + name + "&m=1");
-        }
-        else if(gameMode == Osu.OsuGameMode.CATCHTHEBEAT) {
-            user = REQUEST.callApi(GET_USER + API_KEY + "&u=" + name + "&m=2");
-        }
-        else if(gameMode == Osu.OsuGameMode.MANIA) {
-            user = REQUEST.callApi(GET_USER + API_KEY + "&u=" + name + "&m=3");
-        }
-        else {
-            throw new OsuGamemodeException("[Osu] Inva  lid Gamemode");
-        }
+        //Setting necessary instance variables to call API in update().
+        uname = name;
+        user_ID = 0;
+        gmode = gameMode;
+        key = API_KEY;
+        updateUser();
+    }
 
-        if(user == null) {
-            throw new OsuUserException("[Osu] User not found");
-        } else {
-            user_id = user.getInt("user_id");
-            username = user.getString("username");
-            count300 = user.getInt("count300");
-            count100 = user.getInt("count100");
-            count50 = user.getInt("count50");
-            playcount = user.getInt("playcount");
-            ranked_score = user.getLong("ranked_score");
-            total_score = user.getLong("total_score");
-            pp_rank = user.getInt("pp_rank");
-            level = user.getDouble("level");
-            pp_raw = user.getDouble("pp_raw");
-            accuracy = user.getDouble("accuracy");
-            count_rank_ss = user.getInt("count_rank_ss");
-            count_rank_s = user.getInt("count_rank_s");
-            count_rank_a = user.getInt("count_rank_a");
-            country = user.getString("country");
-            pp_country_rank = user.getInt("pp_country_rank");
-
-            profile_url = "https://osu.ppy.sh/u/" + user_id;
-            avatar_url = "https://a.ppy.sh/" + user_id;
-        }
+    OsuPlayer(String API_KEY, int userID, Osu.OsuGameMode gameMode) throws OsuUserException, IOException, OsuGamemodeException {
+        //Setting necessary instance variables to call API in update().
+        user_ID = userID;
+        gmode = gameMode;
+        key = API_KEY;
+        updateUser();
     }
 
 
-
-    OsuPlayer(String API_KEY, int userID, Osu.OsuGameMode gameMode) throws OsuUserException, IOException, OsuGamemodeException {
-
-        JSONObject user = new JSONObject();
-        if(gameMode == Osu.OsuGameMode.OSU) {
-            user = REQUEST.callApi(GET_USER + API_KEY + "&u=" + userID + "&m=0");
+    /*
+    Check if user has played mode, and iterate what they have done for gamemode
+    Line.69-85
+     */
+    private void updateUser() throws IOException, OsuGamemodeException, OsuUserException {
+        JSONObject user;
+        if(gmode == Osu.OsuGameMode.OSU) {
+            user = REQUEST.callApi(GET_USER + key + "&u=" + (user_ID == 0 ? uname : user_ID) + "&m=0");
         }
-        else if(gameMode == Osu.OsuGameMode.TAIKO) {
-            user = REQUEST.callApi(GET_USER + API_KEY + "&u=" + userID + "&m=1");
+        else if(gmode == Osu.OsuGameMode.TAIKO) {
+            user = REQUEST.callApi(GET_USER + key + "&u=" + (user_ID == 0 ? uname : user_ID) + "&m=1");
         }
-        else if(gameMode == Osu.OsuGameMode.CATCHTHEBEAT) {
-            user = REQUEST.callApi(GET_USER + API_KEY + "&u=" + userID + "&m=2");
+        else if(gmode == Osu.OsuGameMode.CATCHTHEBEAT) {
+            user = REQUEST.callApi(GET_USER + key + "&u=" + (user_ID == 0 ? uname : user_ID) + "&m=2");
         }
-        else if(gameMode == Osu.OsuGameMode.MANIA) {
-            user = REQUEST.callApi(GET_USER + API_KEY + "&u=" + userID + "&m=3");
+        else if(gmode == Osu.OsuGameMode.MANIA) {
+            user = REQUEST.callApi(GET_USER + key + "&u=" + (user_ID == 0 ? uname : user_ID) + "&m=3");
         }
         else {
             throw new OsuGamemodeException("[Osu] Invalid Gamemode");
--- src/test/java/com/crbaldwin/osu_wrapper/OsuPlayerTest.java
+++ src/test/java/com/crbaldwin/osu_wrapper/OsuPlayerTest.java
@@ -9,8 +9,8 @@ public class OsuPlayerTest {
     public static void main(String[] args) throws IOException {
         Osu osu = new Osu("319e7d936f399ab7663781d5bf19858fdc04b2a8");
         try {
-            OsuPlayer player = osu.getPlayer("charless", Osu.OsuGameMode.TAIKO);
-            System.out.println(player.getPPRaw());
+            OsuPlayer player = osu.getPlayer("cookiezi", Osu.OsuGameMode.OSU);
+            System.out.println(player.getAvatarUrl());
         } catch (OsuUserException e) {
             e.printStackTrace();
         } catch (OsuGamemodeException e) {
